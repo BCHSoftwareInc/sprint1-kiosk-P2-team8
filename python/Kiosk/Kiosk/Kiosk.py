7cbf279ac78ff1d90addcbf03fe9f3ca93f5b902
Artdev76 <awarn245@learner.dvusd.org>
2026-08-25 09:24:41 -0700

    Started program

---
+++ python/Kiosk/Kiosk/Kiosk.py
@@ -0,0 +1,15 @@
+print ("What is your name?")
+name=input()
+print (name)
+print ("What is your email?")
+email=input()
+print (email)
+print ("+--------------------------------------------+")
+print ("|            Apex entertainment pass         |")
+print ("|--------------------------------------------|")
+print ("| ATTENDEE : "+(name).ljust(32)+"|")
+
+
+
+
+print ("+--------------------------------------------+")
